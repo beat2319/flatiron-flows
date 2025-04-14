3df2da39726853ba60d3765327cff8b1b2110863
beat2319 <beat2319@colorado.edu>
2025-04-14 09:26:56 -0600

    updated directory folders

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
+
 const path = require('path');
-
 const dbPath = path.join(__dirname, 'data', 'bike_logs.db');
-
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
