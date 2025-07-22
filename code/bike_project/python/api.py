2d3c3bb6b9ed87857de438ffa46df180db0c8b25
beat2319 <beat2319@colorado.edu>
2025-07-22 14:19:24 -0600

    added webscraping, and worked with parse_json logic

--- code/bike_project/python/api.py
+++ code/bike_project/python/api.py
@@ -1,6 +1,11 @@
 import requests
 import numpy as np
+from bs4 import BeautifulSoup
+import pandas as pd
 
+# boulder_weather = https://api.weather.gov/gridpoints/BOU/54,74/forecast/hourly
+cu_campus_weather = "https://sundowner.colorado.edu/weather/atoc1/"
+#
 bcycle_url = "https://gbfs.bcycle.com"
 
 def get_bcycle_json(name):
@@ -33,19 +38,40 @@ status_columns = [
     'num_bikes_available'
 ]
 
-for i in range(len(request_names)):
-    bcycle_json = get_bcycle_json(request_names[i])
-    if bcycle_json:
-        for j in range(len(station_array)):
-            if i == 0:
-                for k in range(len(info_columns)):
-                    print(station_array[j], "==", bcycle_json["data"]["stations"][station_array[j]][info_columns[k]])
-            if i == 1:
-                for k in range(len(status_columns)):
-                    print(station_array[j], "==", bcycle_json["data"]["stations"][station_array[j]][status_columns[k]])
-
 # if bcycle_json:
 #     for i in range(len(station_array)):
 #         for j in range(len(status_columns)):
 #             print(station_array[i], "==", bcycle_json["data"]["stations"][station_array[i]][status_columns[j]])
         # print("index:", station_array[i], bcycle_info["data"]["stations"][station_array[i]]["name"])
+
+def parse_json():
+    for i in range(len(request_names)):
+        bcycle_json = get_bcycle_json(request_names[i])
+        if bcycle_json:
+            for j in range(len(station_array)):
+                if i == 0:
+                    for k in range(len(info_columns)):
+                        print(station_array[j], "==", bcycle_json["data"]["stations"][station_array[j]][info_columns[k]])
+                if i == 1:
+                    for k in range(len(status_columns)):
+                        print(station_array[j], "==", bcycle_json["data"]["stations"][station_array[j]][status_columns[k]])
+
+weather_array = np.array([[0,5,8],
+                         ['temp', 'wind speed', 'total rain']], dtype=object)
+
+def scrape():
+    url = cu_campus_weather
+    response = requests.get(url).content
+    df = pd.read_html(response)[0]
+    df_columns = df.columns[1]
+
+    #print(df_columns)
+    for i in range(len(weather_array[0])):
+        print(weather_array[1][i], df[df_columns][weather_array[0][i]])
+    # df_list = pd.read_html(response)[0]
+    # df_list.reset_index()
+    # print(df_list.columns)
+
+if __name__ == '__main__':
+    scrape()
+    parse_json()
