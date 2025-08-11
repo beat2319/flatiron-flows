3f6fc2eb18466208c93c640b5014e350b8100f4e
beat2319 <beat2319@colorado.edu>
2025-08-11 13:18:02 -0600

    ready to move the repo to the server and start logging the data

--- code/bike_project/python/bike_logs.py
+++ code/bike_project/python/bike_logs.py
@@ -5,6 +5,7 @@ import pandas as pd
 import datetime as dt
 import time
 import sqlite3
+import schedule 
 
 #boulder_weather = https://api.weather.gov/gridpoints/BOU/54,74/forecast/hourly
 precip_url = "https://api.open-meteo.com/v1/forecast?latitude=40.0073&longitude=-105.2660&current=precipitation"
@@ -12,12 +13,12 @@ weather_url = "https://sundowner.colorado.edu/weather/atoc1/"
 bcycle_url = "https://gbfs.bcycle.com/bcycle_boulder/"
 
 # set as object to trick pandas to preserve datatype
-df = pd.DataFrame({
+log_df = pd.DataFrame({
     'station_id':pd.Series([], dtype = 'object'), 
-    'name':pd.Series([], dtype = 'object'), 
-    'lon':pd.Series([], dtype = 'object'), 
-    'lat':pd.Series([], dtype = 'object'),  
-    'num_docks_available':pd.Series([], dtype = 'object'),  
+    # 'name':pd.Series([], dtype = 'object'), 
+    # 'lon':pd.Series([], dtype = 'object'), 
+    # 'lat':pd.Series([], dtype = 'object'),  
+    # 'num_docks_available':pd.Series([], dtype = 'object'),  
     'num_bikes_available':pd.Series([], dtype = 'object'),  
     'temp':pd.Series([], dtype = 'object'),  
     'wind_speed':pd.Series([], dtype = 'object'),  
@@ -42,35 +43,25 @@ def get_bcycle_json(name):
     else:
         print(f"Failed to retrive data {response.status_code}")
 
-info_columns = ['station_id', 'name', 'lon', 'lat']
-
 # this function parse the station info json 
 # only returns the chosen info columns as df
-def parse_info():
+def parse_info(df):
     bcycle_json = get_bcycle_json(request_names[0])
     if bcycle_json:
         for i in range(len(station_array)):
-            if info_columns[0] == df.columns[0]:
-                df.at[i, df.columns[0]] = bcycle_json["data"]["stations"][station_array[i]][info_columns[0]]
-            if info_columns[1] == df.columns[1]:
-                df.at[i, df.columns[1]] = bcycle_json["data"]["stations"][station_array[i]][info_columns[1]]
-            if info_columns[2] == df.columns[2]:
-                df.at[i, df.columns[2]] = bcycle_json["data"]["stations"][station_array[i]][info_columns[2]]
-            if info_columns[3] == df.columns[3]:
-                df.at[i, df.columns[3]] = bcycle_json["data"]["stations"][station_array[i]][info_columns[3]]
-
-status_columns = ['num_docks_available', 'num_bikes_available']
+            if 'station_id' == df.columns[0]:
+                df.at[i, df.columns[0]] = bcycle_json["data"]["stations"][station_array[i]]['station_id']
 
 # this function parse the station status json 
 # only returns the chosen status columns as df
-def parse_status():
+def parse_status(df):
     bcycle_json = get_bcycle_json(request_names[1])
     if bcycle_json:
         for i in range(len(station_array)):
-            if status_columns[0] == df.columns[4]:
-                df.at[i, df.columns[4]] = bcycle_json["data"]["stations"][station_array[i]][status_columns[0]]
-            if status_columns[1] == df.columns[5]:
-                df.at[i, df.columns[5]] = bcycle_json["data"]["stations"][station_array[i]][status_columns[1]]
+            if 'num_bikes_available' == df.columns[1]:
+                df.at[i, df.columns[1]] = bcycle_json["data"]["stations"][station_array[i]]['num_bikes_available']
+            # if status_columns[1] == df.columns[2]:
+            #     df.at[i, df.columns[2]] = bcycle_json["data"]["stations"][station_array[i]][status_columns[1]]
 
 # parses throught the scraped table as a dataframe
 # and verifies the respose code status
@@ -90,17 +81,17 @@ weather_array = np.array([[0,5,8],
 
 # this function takes in the weather table as dataframe
 # adds the parsed temp, wind and rain to the dataframe
-def parse_weather():
+def parse_weather(df):
     scrape_df = get_weather_table(0)
     if not scrape_df.empty:
         columns = scrape_df.columns[1]
         for i in range(len(station_array)):
-            if weather_array[1][0] == df.columns[6]:
-                df.at[i, df.columns[6]] = scrape_df[columns][weather_array[0][0]]
-            if weather_array[1][1] == df.columns[7]:
-                df.at[i, df.columns[7]] = scrape_df[columns][weather_array[0][1]]
-            if weather_array[1][2] == df.columns[8]:
-                df.at[i, df.columns[8]] = scrape_df[columns][weather_array[0][2]]
+            if weather_array[1][0] == df.columns[2]:
+                df.at[i, df.columns[2]] = scrape_df[columns][weather_array[0][0]]
+            if weather_array[1][1] == df.columns[3]:
+                df.at[i, df.columns[3]] = scrape_df[columns][weather_array[0][1]]
+            if weather_array[1][2] == df.columns[4]:
+                df.at[i, df.columns[4]] = scrape_df[columns][weather_array[0][2]]
 
 def get_precip_json():
     url = f"{precip_url}"
@@ -112,38 +103,39 @@ def get_precip_json():
     else:
         print(f"Failed to retrive data {response.status_code}")
 
-def parse_precip():
+def parse_precip(df):
     precip_json = get_precip_json()
     if precip_json:
         for i in range(len(station_array)):
-            df.at[i, df.columns[9]] = precip_json["current"]["precipitation"]
+            df.at[i, df.columns[5]] = precip_json["current"]["precipitation"]
 
 # this function gets the current datetime 
 # and adds date and time to the dataframe
-def parse_datetime():
+def parse_datetime(df):
     now = dt.datetime.now()
     for i in range(len(station_array)):
-        df.at[i, df.columns[10]] = now.strftime('%y%m%d%H%M%S')
+        df.at[i, df.columns[6]] = now.strftime('%y%m%d%H%M%S')
+
+def log_data(df):
+    parse_weather(df)
+    parse_info(df)
+    parse_status(df)
+    parse_datetime(df)
+    parse_precip(df)
+    conn = sqlite3.connect('bike_logs.db')
+    df.to_sql(name='bike_logs', con=conn, if_exists='append', index=False)
+    
 
 
 
 
 if __name__ == '__main__':
-    start = time.time()
-
-    parse_weather()
-    parse_info()
-    parse_status()
-    parse_datetime()
-    parse_precip()
-
-    end = time.time()
-    print(end - start)
     
-    print(df)
-    conn = sqlite3.connect('new_bike_logs.db')
-    c = conn.cursor()
-    df.to_sql(name='bike_logs', con=conn, if_exists='append', index=False)
+    schedule.every(5).minutes.do(log_data(log_df))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
     #print(df.dtypes)
     # parse_json()
     # print(df.head(5))
